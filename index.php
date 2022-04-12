3ca4e453844bb4d75b48e421761ab9c60f2b250d
Martino888 <maxmartino33@gmail.com>
2022-04-12 16:30:47 +0200

    inserimento struttura array

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> php-google-faq</title>
+</head>
+<body>
+    <?php
+        $array_google=[
+            [
+                'argomento'=> "<h1>Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h1>",
+                'risposta' => "<p> </p>
+                                <p> </p>
+                                <p> </p>
+                                <p> </p>
+                                <p> </p>"
+            ],
+        ]
 
+
+    ?>
+</body>
+</html>
